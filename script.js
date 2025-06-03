5242c952546d85e5cc23ee0a95d46495cea35dca
Mashayekhi <www.mm.mashayekhi1988@gmail.com>
2025-06-03 19:53:46 +0330

    start

---
+++ script.js
@@ -0,0 +1,3 @@
+
+
+let arraySlider =  ["image/slider/1.webp", "image/slider/2.jpg","image/slider/3.jpeg", "image/slider/4.webp", "image/slider/5.jpg", "image/slider/6.jpg", "image/slider/7.jpg", "image/slider/8.jpeg", "image/slider/9.jpg", "image/slider/10.jpg", "image/slider/11.jpg", "image/slider/12.jpg", ]
