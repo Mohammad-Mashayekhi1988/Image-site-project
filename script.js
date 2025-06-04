c6e6d784e927dcc86343e4cf2119f1c0d5bdd3b3
Mashayekhi <mm.mashayekhi1988@gmail.com>
2025-06-04 13:02:15 +0330

    add search bar

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelectorAll(".icon").forEach(icon => {
     const text = icon.getAttribute("data-tooltip");
     tooltip.textContent = text;
     tooltip.style.opacity = 1;
-    tooltip.style.transform = "translate(-50%, -100%) scale(0.95)"; // بیاد بالا
+    tooltip.style.transform = "translate(-50%, -200%) scale(0.95)"; // بیاد بالا
   });
 
   icon.addEventListener("mousemove", (e) => {
@@ -17,11 +17,11 @@ document.querySelectorAll(".icon").forEach(icon => {
 
   icon.addEventListener("mouseleave", () => {
     tooltip.style.opacity = 0;
-    tooltip.style.transform = "translate(-50%, 10px) scale(0.95)"; // بره پایین و محو بشه
+    tooltip.style.transform = "translate(-50%, 50px) scale(0.95)"; // بره پایین و محو بشه
   });
 });
 
-
+let slider=document.querySelector('img-slider')
 
 let arraySlider =  ["image/slider/1.webp", "image/slider/2.jpg","image/slider/3.jpeg", "image/slider/4.webp", "image/slider/5.jpg", "image/slider/6.jpg", "image/slider/7.jpg", "image/slider/8.jpeg", "image/slider/9.jpg", "image/slider/10.jpg", "image/slider/11.jpg", "image/slider/12.jpg", ]
 
